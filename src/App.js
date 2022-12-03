1b6df3866935998aec4dfde0c4c83fe1f9c8833c
vikashiscoder <39021552+vikashiscoder@users.noreply.github.com>
2022-12-03 16:51:55 +0000

    first

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./style.css";
+import Login from "./components/login";
 
 export default function App() {
   return (
     <div>
+      <Login></Login>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
